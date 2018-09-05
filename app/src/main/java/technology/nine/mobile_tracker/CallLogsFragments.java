01628f3a7a8a022d5979f4656dfcd6ca5f70db44
yeshwanthmunisifreddy <myeswanthr@gmail.com>
2018-09-05 20:14:12 +0530

    navigation drawer is added

---
+++ app/src/main/java/technology/nine/mobile_tracker/CallLogsFragments.java
@@ -0,0 +1,87 @@
+package technology.nine.mobile_tracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import technology.nine.mobile_tracker.adapters.CallLogsRecyclerAdapter;
+import technology.nine.mobile_tracker.data.CallLogsDBHelper;
+import technology.nine.mobile_tracker.model.CallLogs;
+import technology.nine.mobile_tracker.service.CallDetectService;
+import technology.nine.mobile_tracker.service.SmsDetectingService;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class CallLogsFragments extends Fragment {
+    View view;
+    CallLogsDBHelper helper;
+    RecyclerView recyclerView;
+    CallLogsRecyclerAdapter adapter;
+    LinearLayoutManager linearLayoutManager;
+    List<CallLogs> callLogs = new ArrayList<>();
+    private static final int READ_CALL_LOG_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
+    Intent mServiceIntent;
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Log.e("OnReceive", "is called");
+            fetch(context);
+        }
+    };
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_call_log,container,false);
+        Log.e("onCreate", "is called");
+        recyclerView = view.findViewById(R.id.recycler_view);
+        fetch(getContext());
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e("onStart", "is called");
+        LocalBroadcastManager.getInstance(Objects.requireNonNull(getContext()))
+                .registerReceiver(receiver, new IntentFilter(CallDetectService.UPDATE_UI));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        fetch(getContext());
+    }
+    private void fetch(Context context) {
+        // Log.e("Fetch", "is called");
+        helper = new CallLogsDBHelper(context);
+        callLogs = helper.getAllCallLog();
+        linearLayoutManager = new LinearLayoutManager(context);
+        adapter = new CallLogsRecyclerAdapter(context);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter.addAll(callLogs);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
+    }
+}
---
+++ app/src/main/java/technology/nine/mobile_tracker/NotificationLogFragment.java
@@ -0,0 +1,19 @@
+package technology.nine.mobile_tracker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationLogFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_call_log,container,false);
+        return view;
+    }
+}
